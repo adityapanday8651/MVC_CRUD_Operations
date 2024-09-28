693826549d87e6dcb8ef62924fa8c5ebc4bc9d5c
Aditya Pandey <adityapanday8651@gmail.com>
2024-09-28 15:28:46 +0530

    Add project files.

---
+++ MVCCrudExample/DataContext/ApplicationDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCCrudExample.Models;
+
+namespace MVCCrudExample.DataContext
+{
+    public class ApplicationDataContext : DbContext
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options): base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
